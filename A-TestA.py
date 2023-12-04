9c8b689a873e8db333666a8a305e636fd3d17ff8
Ashis18 <95433091+Ashis18@users.noreply.github.com>
2023-12-04 07:44:41 -0500

    Add files via upload

---
+++ A-TestA.py
@@ -0,0 +1,95 @@
+
+#print(str("no")+" ", int(4*6), " " + str("hello"), int(7+8),float(9/6),str(" are not the same so you are: "), bool(False))
+#print("no.",24,",",15,"is not", 1.5, "so you are: ", False)
+#Print heheheh name hehehe
+#print("remember boy! make that base strong boy make it stronggggggg!!!")
+#how to print on, py consol, terminal, run?
+#this comment color is boring there a way to change it?
+#print("Remember boy! MAKE THAT BASE STRONG BOY MAKE IT STRONGGGGGGG!!!")
+#print(type(6.1))
+
+#A = 9
+#B = "7"
+#print(A + int(B), B)
+
+#A, E, I, O, U = "A", 2, 2.3,"is: ", False
+#print(A,E,I,O,U)
+
+#Print = "Remember boy! MAKE THAT BASE STRONG MAKE IT STRONGGGGG!!!"
+#print(len(Print))
+#print(Print * 5)
+
+#a = "only"
+#print(len(a),a*2)
+
+#we Practicing so sht aint forgotten
+#print(2*3,"3")
+#print(False, "2 does not =", 2, " just like 1.6 is not", 1.6)
+#a = False
+#print("True and " + str(a))
+#a = 30
+#b = 3
+## answer = a/b suspect cause of why wrong answers were accepted as true.
+#answer = 30/3
+#input("Please answer the question: ")
+#if answer = a/b:
+#if answer = 10:
+#if answer == 10
+#if answer >= 10:
+ #   print(bool(True))
+#else :print(bool(False))
+
+#IDK wdf i did wrong but this aint it. THIS SHT KEEPS PRINTING TRUE EVEN FOR FALSE ANSWERS AAAAAAA
+#ight we raged quit last time anyhow we back :D
+
+#ans = input("What is 30 divided by 3? ")
+
+#a, b, = 30, 3
+#if ans == 10:
+#    print(bool(True))
+#else:print(bool(False))
+
+#LOL EXS DEEEEEE
+#ight so lets make a test :D
+#okie so lets start right...umm..i wanna just write one YES I KNOW HOW TO WRITE IT OK IF ELSE LES GO;
+#okie so all have to have one commonality which is the answer but how do i make it so each question = answer
+#q1 = a
+#q2 = a
+#q3 = a
+#ok so what i can do is make a multi  layered if else
+#hmmm lets work on it the 4 lines below is what lead to this rant.
+
+#answer = input("answer: ")
+#answer_a = input("Answer: ")
+#if answer = "hello":
+ #   print()
+ #q1,q2,q3 = ""
+a1, a2, a3 = "i purr", "i blob", "i woof"
+#dang it how do i put this answer in a row...columbs AAAAAAAAAAA ight les look it up :'D
+
+answer_a = input("Please answer in lowercase: ")
+#if answer_a = cat:
+#if answer_a = "cat":
+if answer_a == "cat":
+    print(bool(True))
+#elif answer_a = "dog"
+    #elif answer_a =
+elif answer_a=="dog":
+    print(bool(True))
+elif answer_a=="fish":
+    print(bool(True))
+else:print(bool(False))
+
+for i in range(0,2):
+#    print(a1,a2,a3)
+
+#ight so i feel like am on to something but like i printed aaa whatever u guys go back and check m too lazy to go back..i mean sleepy ;c its 5:26 AM no sleep
+#a syntax...this is a syntax: else:print(bool(false)) something like this would be a syntax error: else print(bool(False)) ;D
+
+
+
+
+
+
+
+#PRINT("LEARN SOMETHINGS HISTORY FIRST BEFORE EMBARKING ON IT")
